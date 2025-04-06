a5cf3fd2717f541f02de5771e5bd8dedd871564d
ljh4770 <ljh4770@naver.com>
2025-04-06 16:54:00 +0900

    feat(api): dummy reply implemented

--- api/route/rag.py
+++ api/route/rag.py
@@ -5,3 +5,9 @@ router = APIRouter(prefix="/rag", tags=['RAG'])
 @router.get('/')
 def greet():
     return "RAG api"
+
+@router.post('/')
+def reply():
+    return {
+        "reply": "Reply from rag server"
+    }
