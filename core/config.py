4acb4d74ee60e9e92e023af63f91751982665ff8
ljh4770 <ljh4770@naver.com>
2025-04-09 19:10:25 +0900

    feat(rag): add OpenAI API key

--- core/config.py
+++ core/config.py
@@ -1,7 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-
+    OPENAI_API_KEY: str
+    
     # Load contents of .env file
     model_config = SettingsConfigDict(
         env_file=".env",
