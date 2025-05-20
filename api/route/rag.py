c109e4933563f48e4c6a2dd58cf561b79c5527bc
ljh4770 <ljh4770@naver.com>
2025-05-21 01:32:14 +0900

    feat(RAG) - reply with Title&Ticker

--- api/route/rag.py
+++ api/route/rag.py
@@ -21,37 +21,28 @@ async def reply(req: QueryRequest):
     if not user_query:
         return {"error": "Missing 'query' in request body"}
 
-    # Create LLM Tasks
-    # 1) 항상 실행할 본문·티커 태스크
+    # Call chains in parallel
     reply_task = chain.ainvoke(user_query)
     ticker_task = chain_ticker.ainvoke(user_query)
-
-    # 2) is_first_chat에 따라 제목 태스크만 추가
-    if is_first_chat:
+    if is_first_chat == True: # Need to create a chat session title
         title_task = chain_title.ainvoke(user_query)
-        # 세 개의 태스크를 병렬 실행
+        # Execute in parallel
         title, reply, ticker = await asyncio.gather(
             title_task,
             reply_task,
             ticker_task
         )
     else:
-        # 제목 태스크 없이 본문·티커만 병렬 실행
+        # Execute in parallel
         reply, ticker = await asyncio.gather(
             reply_task,
             ticker_task
         )
-        title = None  # 또는 "" 등 원하는 기본값
-
-    if ticker == "False":
-        pass
-        # skip the attribute for events and datetime
-    else:
-        pass
-        # Fetch the events and datetime
+        title = None
     
     res = QueryResponse(
         title= title,
-        reply=reply
+        reply=reply,
+        ticker=ticker
     )
     return res
