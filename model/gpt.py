463a943191fe29ac2852b93b72d411875e77210c
ljh4770 <ljh4770@naver.com>
2025-05-20 21:28:12 +0900

    feat - Chain for creating title

--- model/gpt.py
+++ model/gpt.py
@@ -8,14 +8,30 @@ from core.config import settings
 # Prompt
 template = "Answer the question: Question: {question}"
 
+template_title = (
+    "Read the user's question and generate a concise, descriptive title that represents it.\n"
+    "Question: {question}\n"
+    "Title:"
+)
+
 prompt = ChatPromptTemplate.from_template(template)
+prompt_title = ChatPromptTemplate.from_template(template_title)
 
 # LLM
 model = ChatOpenAI(model='gpt-4o-mini', temperature=0, api_key=settings.OPENAI_API_KEY)
 
+# Answer the query
 chain = (
     {'question': RunnablePassthrough()}
     | prompt
+    | model
+    | StrOutputParser()
+)
+
+# Create a title for the query
+chain_title = (
+    {'question': RunnablePassthrough()}
+    | prompt_title
     | model
     | StrOutputParser()
 )
